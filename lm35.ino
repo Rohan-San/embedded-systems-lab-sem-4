0f78ea02084bcca51f9ed586f8cccf2a54e7db0c
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-07-21 01:38:57 +0530

    Shorter variables

--- lm35.ino
+++ lm35.ino
@@ -7,12 +7,12 @@ void setup() {
 void loop() {
     int reading = analogRead(sensorPin);
     float voltage = reading * (5.0 / 1024.0);
-    float temperatureC = voltage * 100;
+    float tempC = voltage * 100;
     Serial.print("Temperature: ");
-    Serial.print(temperatureC);
+    Serial.print(tempC);
     Serial.print("°C  |  ");
-    float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
-    Serial.print(temperatureF);
+    float tempF = (tempC * 9.0 / 5.0) + 32.0;
+    Serial.print(tempF);
     Serial.println("°F");
     delay(1000);
 }
